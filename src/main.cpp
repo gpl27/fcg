e4e01da2d2fdeb5c13949a11c492b6bd67f0032e
Mateus Kienzle <mateuskienzle@gmail.com>
2024-08-17 04:01:11 -0300

    Add free camera

--- src/main.cpp
+++ src/main.cpp
@@ -163,6 +163,9 @@ float g_CameraTheta = 0.0f; // Ângulo no plano ZX em relação ao eixo Z
 float g_CameraPhi = 0.0f;   // Ângulo em relação ao eixo Y
 float g_CameraDistance = 3.5f; // Distância da câmera para a origem
 
+// Variável que controla o tipo de câmera utilizada: look_at ou free_cam.
+bool g_CameraOption = false;
+
 // Variável que controla o tipo de projeção utilizada: perspectiva ou ortográfica.
 bool g_UsePerspectiveProjection = true;
 bool g_FPV = false;
@@ -213,18 +216,20 @@ class Entity {
         }
         void update(double delta) {
             // User interaction
-            velocity.z = (g_KeyW_Pressed)? 3 : 0;
-            velocity.z = (g_KeyS_Pressed)? -3 : velocity.z;
-            velocity.x = (g_KeyD_Pressed)? -3 : 0;
-            velocity.x = (g_KeyA_Pressed)? 3 : velocity.x;
-            // Gravity
+            if (!g_CameraOption){
+                velocity.z = (g_KeyW_Pressed)? 3 : 0;
+                velocity.z = (g_KeyS_Pressed)? -3 : velocity.z;
+                velocity.x = (g_KeyD_Pressed)? -3 : 0;
+                velocity.x = (g_KeyA_Pressed)? 3 : velocity.x;
+                // Gravity
 
-            // Check for collisions
+                // Check for collisions
 
-            // Update position
-            pos.x += (velocity.x*delta);
-            pos.y += (velocity.y*delta);
-            pos.z += (velocity.z*delta);
+                // Update position
+                pos.x += (velocity.x*delta);
+                pos.y += (velocity.y*delta);
+                pos.z += (velocity.z*delta);
+            }
         }
 };
 
@@ -368,9 +373,18 @@ int main(int argc, char* argv[])
 
     double curr_t = glfwGetTime();
 
+    float delta_time = 0.0f;
+    float freeCam_speed = 2.5f;
+    float time = (float) glfwGetTime();
+
     Entity* mario;
     std::vector<Entity> entities;
     LoadMap("../../src/map.txt", entities, mario); // Função que abre arquivo .txt para carregar o mapa do jogo
+
+    glm::vec4 camera_position_c  = glm::vec4(mario->pos.x+1.0f,mario->pos.y+5.0f,mario->pos.z,1.0f); // Ponto "c", centro da câmera
+    glm::vec4 camera_free    = glm::vec4(mario->pos.x,mario->pos.y,mario->pos.z,1.0f); // Ponto "l", para onde a câmera (look-at) estará sempre olhando
+    glm::vec4 camera_view_vector = camera_free - camera_position_c; // Vetor "view", sentido para onde a câmera está virada
+    glm::vec4 camera_up_vector   = glm::vec4(0.0f,1.0f,0.0f,0.0f); // Vetor "up" fixado para apontar para o "céu" (eito Y global)
 
     while (!glfwWindowShouldClose(window))
     {   
@@ -389,11 +403,6 @@ int main(int argc, char* argv[])
         glUseProgram(g_GpuProgramID);
 
         // Abaixo definimos as varáveis que efetivamente definem a câmera virtual.
-        // Veja slides 195-227 e 229-234 do documento Aula_08_Sistemas_de_Coordenadas.pdf.
-        // glm::vec4 camera_position_c  = glm::vec4(x,y,z,1.0f); // Ponto "c", centro da câmera
-        // glm::vec4 camera_lookat_l    = glm::vec4(0.0f,0.0f,0.0f,1.0f); // Ponto "l", para onde a câmera (look-at) estará sempre olhando
-        // glm::vec4 camera_view_vector = camera_lookat_l - camera_position_c; // Vetor "view", sentido para onde a câmera está virada
-        // glm::vec4 camera_up_vector   = glm::vec4(0.0f,1.0f,0.0f,0.0f); // Vetor "up" fixado para apontar para o "céu" (eito Y global)
 
         // Computamos a matriz "View" utilizando os parâmetros da câmera para
         // definir o sistema de coordenadas da câmera.  Veja slides 2-14, 184-190 e 236-242 do documento Aula_08_Sistemas_de_Coordenadas.pdf.
@@ -405,11 +414,42 @@ int main(int argc, char* argv[])
             float y = r*sin(g_CameraPhi);
             float z = r*cos(g_CameraPhi)*cos(g_CameraTheta);
             float x = r*cos(g_CameraPhi)*sin(g_CameraTheta);
-            glm::vec4 camera_position_c  = glm::vec4(mario->pos.x+x,mario->pos.y+y,mario->pos.z+z,1.0f); // Ponto "c", centro da câmera
-            glm::vec4 camera_lookat_l    = glm::vec4(mario->pos.x,mario->pos.y,mario->pos.z,1.0f); // Ponto "l", para onde a câmera (look-at) estará sempre olhando
-            glm::vec4 camera_view_vector = camera_lookat_l - camera_position_c; // Vetor "view", sentido para onde a câmera está virada
-            glm::vec4 camera_up_vector   = glm::vec4(0.0f,1.0f,0.0f,0.0f); // Vetor "up" fixado para apontar para o "céu" (eito Y global)
-            view = Matrix_Camera_View(camera_position_c, camera_view_vector, camera_up_vector);
+
+            if(g_CameraOption){
+
+                glm::vec4 w = -camera_view_vector/norm(camera_view_vector);
+                glm::vec4 u = crossproduct(camera_up_vector, w)/norm(crossproduct(camera_up_vector, w));
+
+                float now = (float) glfwGetTime();
+                delta_time = now - time;
+                time = now;
+
+                camera_view_vector =  - glm::vec4 (x,y,z, 0.0f);
+
+                if(g_KeyW_Pressed){
+                    camera_position_c -= freeCam_speed*delta_time*w;
+                }
+                if(g_KeyA_Pressed){
+                    camera_position_c -= freeCam_speed*delta_time*u;
+                }
+                if(g_KeyS_Pressed){
+                    camera_position_c += freeCam_speed*delta_time*w;
+                }
+                if(g_KeyD_Pressed){
+                    camera_position_c += freeCam_speed*delta_time*u;
+                }
+
+                view = Matrix_Camera_View(camera_position_c, camera_view_vector, camera_up_vector);
+            }
+            else{
+                glm::vec4 camera_position_c  = glm::vec4(mario->pos.x+x,mario->pos.y+y,mario->pos.z+z,1.0f); // Ponto "c", centro da câmera
+                glm::vec4 camera_lookat_l    = glm::vec4(mario->pos.x,mario->pos.y,mario->pos.z,1.0f); // Ponto "l", para onde a câmera (look-at) estará sempre olhando
+                glm::vec4 camera_view_vector = camera_lookat_l - camera_position_c; // Vetor "view", sentido para onde a câmera está virada
+                glm::vec4 camera_up_vector   = glm::vec4(0.0f,1.0f,0.0f,0.0f); // Vetor "up" fixado para apontar para o "céu" (eito Y global)
+
+                view = Matrix_Camera_View(camera_position_c, camera_view_vector, camera_up_vector);
+            }
+            
         }
 
         glm::mat4 projection;
@@ -1201,6 +1241,20 @@ void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mod)
     if (key == GLFW_KEY_O && action == GLFW_PRESS)
     {
         g_UsePerspectiveProjection = false;
+    }
+
+    // Se o usuário apertar a tecla F, altera para o tipo de câmera free_cam.
+    if (key == GLFW_KEY_F && action == GLFW_PRESS)
+    {
+        g_CameraOption = true;
+        fprintf(stdout,"Troca pra Free cam\n");
+    }
+
+    // Se o usuário apertar a tecla F, altera para o tipo de câmera entre look_at.
+    if (key == GLFW_KEY_L && action == GLFW_PRESS)
+    {
+        g_CameraOption = false;
+        fprintf(stdout,"Troca pra look_at\n");
     }
 
     // Se o usuário apertar a tecla H, fazemos um "toggle" do texto informativo mostrado na tela.
